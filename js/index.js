813e9f291db62dde6e416b2e2519800049ccc46b
unknown <kevinmalaverr@gmail.com>
2020-08-04 19:28:16 -0500

    paginacion

--- js/index.js
+++ js/index.js
@@ -1,19 +1,31 @@
 $(document).ready(async () => {
 	animateCards();
+	pagination();
 });
 
-// funcion para poder animar las tarjetas cuando cargan
+function pagination() {
+	$('#page-selection')
+		.bootpag({
+			total: 5,
+		})
+		.on('page', function (event, num) {
+			// cargar contetenido de acuerdo a el número de pagina
+			animateCards();
+			$(this).bootpag({ total: 10, maxVisible: 5 });
+		});
+}
+
 async function animateCards() {
 	const $cards = $('.article-card'); // referencia a los elementos
-	$cards.hide(); // ocultamos los elementos para poder aplicar fadeIn()
+	$cards.fadeTo(0, 0); // se estable la opacidad a 0
 
 	// promesa para poder esperar luego de cada ejecucion del ciclo
 	const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
-	// ciclo donde se aplica fadeIn() a cada elemento
+	// ciclo donde se ejecuta fade a cada elemento para que se muestre
 	for (let i = 0; i < $cards.length; i++) {
 		console.log(i);
-		$($cards[i]).fadeIn(300);
+		$($cards[i]).fadeTo(300, 1);
 		await delay(200);
 	}
 }
